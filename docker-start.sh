0cfa3633660adc43d268901fcdcabd3c38ee195c
web3gaoyutang <120102857+web3gaoyutang@users.noreply.github.com>
2023-09-25 22:22:41 +0800

    Create docker-start.sh

---
+++ docker-start.sh
@@ -0,0 +1,8 @@
+curl https://get.docker.com | sh \ && sudo systemctl --now enable docker
+sudo /lib/systemd/systemd-sysv-install enable docker
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+sudo systemctl restart docker
